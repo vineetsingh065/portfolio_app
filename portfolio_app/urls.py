9f591e22da4839769371bc4be0d6a0b02c7651a8
vineetsingh065@gmail.com <Hunt123@>
2020-08-29 16:56:34 +0530

    events added

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r"admin/", admin.site.urls),
+    url(r"portfoliomainadmin/", admin.site.urls),
     url(r"", include("portfolio_main.urls")),
 ]
 
